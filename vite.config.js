51b75915838c191933d5431bfa7b3ceb829eb381
budderrar <ahmednasser82380@gmail.com>
2024-11-08 21:39:33 +0200

    Made changes in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Apple_Clone/",
+  base: "/apple_clone/",
   plugins: [react(),({
     org: "no-organization-cw3",
     project: "javascript-react"
