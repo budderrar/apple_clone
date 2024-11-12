b2a9abd253c1c7bd7b24cb290682436f355b01d9
budderrar <ahmednasser82380@gmail.com>
2024-11-12 17:59:51 +0200

    fix and error in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/apple_clone/",
   plugins: [react(),({
     org: "no-organization-cw3",
     project: "javascript-react"
